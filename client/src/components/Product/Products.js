332b94ced52bcd13121bba640c3f6a62678593e5
harsh gupta <118052863+Harsh605@users.noreply.github.com>
2023-04-11 12:10:20 +0530

    Add files via upload

--- client/src/components/Product/Products.js
+++ client/src/components/Product/Products.js
@@ -1,29 +1,48 @@
 import React, { Fragment, useEffect, useState } from 'react'
 import ProductCard from '../Home/ProductCard'
 import { useDispatch, useSelector } from "react-redux"
-import { getProductsApi } from '../../slices/ProductSlice'
+import { getFilterProductsApi } from '../../slices/ProductSlice'
 import "./Products.css"
 import Pagination from "react-js-pagination"
 import { useParams } from "react-router-dom";
 // import MetaData from "../layout/MetaData";
+import Slider from '@mui/material/Slider';
+import Typography from '@mui/material/Typography';
+
+
+const categories = [
+    "electronics",
+    "clothes",
+    "vehicle",
+    "grocery",
+    "home"
+
+
+
+]
 
 const Products = () => {
     const [currentPage, setCurrentPage] = useState(1)
+    const [price, setPrice] = useState([0, 500000]);
+    const [category, setCategory] = useState("");
+    const [ratings, setRatings] = useState(0);
+    
     const dispatch = useDispatch();
     const { keyword } = useParams();
-    
-    const { data, isLoading } = useSelector((state) => state.custom)
-    const { resultPerPage, productsCount } = data
+
+    const { filterProductsdata, isLoading } = useSelector((state) => state.custom)
+    const { resultPerPage, productsCount } = filterProductsdata
 
     const setCurrentPageNo = (e) => {
         setCurrentPage(e)
     }
-    console.log(currentPage)
+    const priceHandler = (event, newPrice) => {
+        setPrice(newPrice);
+    };
 
     useEffect(() => {
-        dispatch(getProductsApi(keyword, currentPage))
-    }, [keyword, currentPage])
-    console.log(currentPage)
+        dispatch(getFilterProductsApi({keyword, currentPage, price, category,ratings }))
+    }, [keyword, currentPage, price, category,ratings])
 
 
     return (
@@ -34,11 +53,49 @@ const Products = () => {
                 <Fragment>
                     <h2 className="productsHeading">Products</h2>
                     <div className="products">
-                        {data.products && data.products.map((curPro) => (
+                        {filterProductsdata.products && filterProductsdata.products.map((curPro) => (
                             <ProductCard key={curPro._id} product={curPro} />
                         ))
                         }
                     </div>
+
+                    <div className="filterBox">
+                        <Typography>Price</Typography>
+                        <Slider
+                            value={price}
+                            onChange={priceHandler}
+                            valueLabelDisplay="auto"
+                            aria-labelledby="range-slider"
+                            min={0}
+                            max={500000}
+                        />
+                        <Typography>Categories</Typography>
+                        <ul className="categoryBox">
+                            {categories.map((category) => (
+                                <li
+                                    className="category-link"
+                                    key={category}
+                                    onClick={() => setCategory(category)}
+                                >
+                                    {category}
+                                </li>
+                            ))}
+                        </ul>
+                        <fieldset>
+                            <Typography component="legend">Ratings Above</Typography>
+                            <Slider
+                                value={ratings}
+                                onChange={(e, newRating) => {
+                                    setRatings(newRating);
+                                }}
+                                aria-labelledby="continuous-slider"
+                                valueLabelDisplay="auto"
+                                min={0}
+                                max={5}
+                            />
+                        </fieldset>
+                    </div>
+
                     {resultPerPage < productsCount && (
                         <div className="paginationBox">
                             <Pagination
--- client/src/slices/ProductSlice.js
+++ client/src/slices/ProductSlice.js
@@ -1,11 +1,23 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 
-export const getProductsApi = createAsyncThunk("getProducts", async (keyword="",currentPage=1) => {
-    // console.log(currentPage)
-    const res = await fetch(`http://localhost:5500/api/v1/product?keyword=${keyword}&page=${currentPage}`)
+export const getProductsApi = createAsyncThunk("getProducts", async () => {
+    let url = `http://localhost:5500/api/v1/product`
+    const res = await fetch(url)
     const result = res.json()
     return result
 })
+
+export const getFilterProductsApi = createAsyncThunk("getFilterProducts", async ({keyword="",currentPage=1,price=[0, 500000],category,ratings=0}) => {
+    let url = `http://localhost:5500/api/v1/product?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&ratings[gte]=${ratings}`
+
+    if(category){
+        url = `http://localhost:5500/api/v1/product?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}&ratings[gte]=${ratings}`
+    }
+    const res = await fetch(url)
+    const result = res.json()
+    return result
+})
+
 export const getSingleProductsApi = createAsyncThunk("getSingleProducts", async (id) => {
     const res = await fetch(`http://localhost:5500/api/v1/product/${id}`)
     const result = res.json()
@@ -17,6 +29,7 @@ export const getProductsSlice = createSlice({
     initialState: {
         isLoading : false,
         data: [],
+        filterProductsdata: [],
         singleProductData:{},
         isError: false
     },
@@ -32,6 +45,18 @@ export const getProductsSlice = createSlice({
         .addCase(getProductsApi.rejected,(state)=>{
             state.isError=true
             state.data=null
+            state.isLoading=false
+        })
+        .addCase(getFilterProductsApi.pending,(state)=>{
+            state.isLoading=true
+        })
+        .addCase(getFilterProductsApi.fulfilled,(state,action)=>{
+            state.isLoading=false
+            state.filterProductsdata=action.payload
+        })
+        .addCase(getFilterProductsApi.rejected,(state)=>{
+            state.isError=true
+            state.filterProductsdata=null
             state.isLoading=false
         })
         .addCase(getSingleProductsApi.pending,(state)=>{
