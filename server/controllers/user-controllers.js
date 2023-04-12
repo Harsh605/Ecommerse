8e4639ace2d888bae7004200a3ca6af0f6fc4ca7
harsh gupta <118052863+Harsh605@users.noreply.github.com>
2023-04-12 19:44:21 +0530

    Add files via upload

--- server/controllers/user-controllers.js
+++ server/controllers/user-controllers.js
@@ -4,15 +4,20 @@ import { catchAsyncError } from "../middleware/catchAsyncError.js"
 import sendToken from "../utils/sendJwtToken.js"
 import sendEmail from "../utils/sendEmail.js"
 import crypto from "crypto"
+import cloudinary from "cloudinary"
 
 export const userRegistration = catchAsyncError(async (req, res, next) => {
     const { name, email, password } = req.body
-
+    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar,{
+        folder:"avatars",
+        width:150,
+        crop:"scale"
+    })
     const user = await User.create({
         name, email, password,
         avatar: {
-            public_id: "this is sample id",
-            url: "profilePicUrl"
+            public_id: myCloud.public_id,
+            url: myCloud.secure_url
         }
     })
     await user.save()
@@ -28,7 +33,7 @@ export const userRegistration = catchAsyncError(async (req, res, next) => {
 
 export const userLogin = catchAsyncError(async (req, res, next) => {
     const { email, password } = req.body
-
+    console.log(email)
     // checking if user gave email and password both
 
     if (!email || !password) {
@@ -40,12 +45,12 @@ export const userLogin = catchAsyncError(async (req, res, next) => {
     if (!user) {
         return next(new ErrorHandler("Invaild Email or Password", 401))
     }
-    
+
     const isPasswordMatch = await user.comparePassword(password)
     if (!isPasswordMatch) {
         return next(new ErrorHandler("Invaild Email or Password", 400))
     }
-    
+
     sendToken(user, 200, res)
 
 })
@@ -63,7 +68,7 @@ export const userLogout = catchAsyncError(async (req, res, next) => {
 })
 
 // forget password
-export const forgetPassword = catchAsyncError(async(req,res,next)=>{
+export const forgetPassword = catchAsyncError(async (req, res, next) => {
     const user = await User.findOne({ email: req.body.email })
 
     if (!user) {
@@ -71,8 +76,8 @@ export const forgetPassword = catchAsyncError(async(req,res,next)=>{
     }
 
     // get reset passwordToken
-    const resetToken =await user.getResetPasswordToken()             //isko url ke sath bej sakte
-    await user.save({validateBeforeSave:false})              //isse hashtoken save ho jyeaga
+    const resetToken = await user.getResetPasswordToken()             //isko url ke sath bej sakte
+    await user.save({ validateBeforeSave: false })              //isse hashtoken save ho jyeaga
 
     const resetPasswordUrl = `${req.protocol}://${req.get("host")}/api/v1/password/reset/${resetToken}`
     console.log(resetPasswordUrl)
@@ -85,95 +90,95 @@ export const forgetPassword = catchAsyncError(async(req,res,next)=>{
             message
         })
         res.status(200).json({
-            success:true,
+            success: true,
             message: `Email send to ${user.email} successfully.`
         })
     } catch (error) {
-        user.resetPasswordToken= undefined
-        user.resetPasswordExpire= undefined
-        await user.save({validateBeforeSave:false})  
-        return next(new ErrorHandler(error.message,500))
+        user.resetPasswordToken = undefined
+        user.resetPasswordExpire = undefined
+        await user.save({ validateBeforeSave: false })
+        return next(new ErrorHandler(error.message, 500))
     }
 })
 
 
 // reset password after user got ResetLink
 
-export const resetPassword = catchAsyncError(async(req,res,next)=>{
+export const resetPassword = catchAsyncError(async (req, res, next) => {
 
     // creating token hash
     const resetPasswordToken = crypto.createHash("sha256").update(req.params.token).digest("hex")
 
     const user = await User.findOne({
         resetPasswordToken,
-        resetPasswordExpire: {$gt: Date.now()}
+        resetPasswordExpire: { $gt: Date.now() }
     })
     if (!user) {
         return next(new ErrorHandler("Reset Password link is invalid or expired", 404))
     }
 
-    if(req.body.password !== req.body.confirmPassword){
+    if (req.body.password !== req.body.confirmPassword) {
         return next(new ErrorHandler("Password and Confirm Password is not same.", 404))
     }
 
     user.password = req.body.password;
-    user.resetPasswordToken= undefined
-    user.resetPasswordExpire= undefined
+    user.resetPasswordToken = undefined
+    user.resetPasswordExpire = undefined
 
     await user.save()
 
-    sendToken(user,200,res)
-   
-    
+    sendToken(user, 200, res)
+
+
 })
 
 // get user profile --apni apni
-export const getUserDetails=catchAsyncError(async(req,res,next)=>{
+export const getUserDetails = catchAsyncError(async (req, res, next) => {
     const user = await User.findById(req.user.id)
 
     res.status(200).json({
-        success:true,
+        success: true,
         user
     })
 })
 
 // update password
-export const updatePassword = catchAsyncError(async(req,res,next)=>{
-   const {oldPassword,newPassword,confirmPassword} = req.body
+export const updatePassword = catchAsyncError(async (req, res, next) => {
+    const { oldPassword, newPassword, confirmPassword } = req.body
 
     const user = await User.findById(req.user.id).select("+password")
 
     const isPasswordMatched = await user.comparePassword(oldPassword)
-    if(!isPasswordMatched){
+    if (!isPasswordMatched) {
         return next(new ErrorHandler("Old Password is not correct", 400))
-    } 
-    if(newPassword!==confirmPassword){
+    }
+    if (newPassword !== confirmPassword) {
         return next(new ErrorHandler("Password and confirm password must be same", 400))
     }
     user.password = newPassword
     await user.save()
-    sendToken(user,200,res)
+    sendToken(user, 200, res)
 })
 
 // update Profile  ------------------------idhar thoda difference h result m error bala
-export const updateProfile = catchAsyncError(async(req,res,next)=>{
+export const updateProfile = catchAsyncError(async (req, res, next) => {
     //we will avatar later
-    const newUserData ={
-        name:req.body.name,
-        email:req.body.email
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email
     }
     console.log(newUserData)
-     const user = await User.findByIdAndUpdate(req.user.id,newUserData,{
-        new:true,
-        runValidators:true,
+    const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+        new: true,
+        runValidators: true,
         useFindAndModify: false
-     })
-   
-     await user.save()
-     res.status(200).json({
-        success:true
-     })
- })
+    })
+
+    await user.save()
+    res.status(200).json({
+        success: true
+    })
+})
 
 // get all users-Admin 
 export const getAllUsers = catchAsyncError(async (req, res, next) => {
@@ -187,7 +192,7 @@ export const getAllUsers = catchAsyncError(async (req, res, next) => {
 //get single user(admin)      //yaani ki ek admin ek ek karke users ki details lo dhekh sakta hai
 export const getSingleUser = catchAsyncError(async (req, res, next) => {
     const user = await User.findById(req.params.id)
-    if(!user){
+    if (!user) {
         return next(new ErrorHandler(`User doesn't exist with id ${req.params.id}`, 400))
     }
     res.status(200).json({
@@ -197,35 +202,35 @@ export const getSingleUser = catchAsyncError(async (req, res, next) => {
 })
 
 // admin kisi bhi user ka role change kar skta
-export const updateUserRole = catchAsyncError(async(req,res,next)=>{
-     const user = await User.findById(req.params.id)
-     if(!user){
+export const updateUserRole = catchAsyncError(async (req, res, next) => {
+    const user = await User.findById(req.params.id)
+    if (!user) {
         return next(new ErrorHandler(`User doesn't exist with id ${req.params.id}`, 400))
     }
-     if(user.role==="admin"){
+    if (user.role === "admin") {
         return next(new ErrorHandler("You are not authorized to update this user.", 403))
-     }
-     user.role= req.body.role
-     await user.save()
-     res.status(200).json({
-        success:true
-     })
- })
+    }
+    user.role = req.body.role
+    await user.save()
+    res.status(200).json({
+        success: true
+    })
+})
 
 //  admin can delete any user
-export const deleteUser=catchAsyncError(async(req,res,next)=>{
+export const deleteUser = catchAsyncError(async (req, res, next) => {
     const user = await User.findById(req.params.id)
-    if(!user){
+    if (!user) {
         return next(new ErrorHandler(`User doesn't exist with id ${req.params.id}`, 400))
     }
-    if(user.role==="admin"){
+    if (user.role === "admin") {
         return next(new ErrorHandler("You are not authorized to delete this user.", 403))
-     }
-     await user.deleteOne()
-     res.status(200).json({
-        success:true,
-        message:"User deleted successfully"
-     })
+    }
+    await user.deleteOne()
+    res.status(200).json({
+        success: true,
+        message: "User deleted successfully"
+    })
 })
 
 
--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ import routes from "./routes/ProductRoutes.js"
 import { Error } from "./middleware/error.js"
 import userRoutes from "./routes/userRoutes.js"
 import cookieParser from "cookie-parser"
+import cloudinary from "cloudinary"
+import bodyParser from "body-parser"
+import fileUpload from "express-fileupload"
+
 dotenv.config()
 
 // unhandled Uncaught Exception   upper hi likhna shi nhi iske upper likha glat to wha se error aa jyega 
@@ -21,9 +25,12 @@ const DB_MONGOOSE = process.env.DB_MONGOOSE
 const PORT = process.env.PORT
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json({
+    limit: '50mb'
+  }))
 app.use(cookieParser())
-app.use(express.urlencoded())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(fileUpload())
 app.use("/api/v1",routes)
 app.use("/api/v1",userRoutes)
 app.use(Error)
@@ -34,6 +41,13 @@ mongoose.connect(DB_MONGOOSE)
         console.log("Database is connected")
     )
       //catch hta diya kyu ki ab humne unhandled error kar liya
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 
 const server= app.listen(PORT,()=>{
