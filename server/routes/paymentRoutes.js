3c03d1536a32b8cfa4609a0d63b34eaf7c1dcab9
harsh gupta <118052863+Harsh605@users.noreply.github.com>
2023-04-19 01:49:19 +0530

    Update paymentRoutes.js

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -2,8 +2,6 @@ import { Router } from "express";
 import { isAuthenticatedUser } from "../middleware/auth.js";
 import { checkout,
     paymentVerification,sendRazorApiKey } from "../controllers/paymentController.js";
-// import { isAuthenticatedUser } from "../middleware/auth.js";
-// import { processPayment, sendStripeApiKey } from "../controllers/paymentController.js";
 
 const paymentRoutes = Router()
 
@@ -13,10 +11,6 @@ const paymentRoutes = Router()
 paymentRoutes.post("/checkout",isAuthenticatedUser,checkout)
 paymentRoutes.post("/paymentVerification",isAuthenticatedUser,paymentVerification)
 paymentRoutes.post("/razorpayApiKey",isAuthenticatedUser,sendRazorApiKey)
-
-// paymentRoutes.post("/stripeApiKey",isAuthenticatedUser,sendStripeApiKey)
-// paymentRoutes.post("/stripeApiKey",isAuthenticatedUser,sendStripeApiKey)
-
 
 
 export default paymentRoutes
